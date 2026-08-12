54fecc1cf9db8aba96b9ab66d79962cf6dc4692a
Tiago Ribeiro <tiago.ribeiro@noirlab.edu>
2026-08-12 11:29:13 -0700

    Update documentation configuration

--- doc/conf.py
+++ doc/conf.py
@@ -3,8 +3,7 @@
 This configuration only affects single-package Sphinx documentation builds.
 """
 
-import lsst.ts.scriptqueue  # noqa
-from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
+from documenteer.conf.guide import *  # type: ignore # noqa
 
 project = "ts_scriptqueue"
 html_theme_options["logotext"] = project  # type: ignore # noqa
